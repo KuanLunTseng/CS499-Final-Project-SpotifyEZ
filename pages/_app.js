cd126f6bf4d4c724287ec649664d50cb30dc069d
KuanLunTseng <tsengku@oregonstate.edu>
2021-03-28 04:48:18 -0700

    removed client id

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,28 @@
-import { Provider } from 'react-redux'
-import { createWrapper } from 'next-redux-wrapper'
+import {Provider} from 'react-redux'
+import {createWrapper} from 'next-redux-wrapper'
 import store from '../store/store'
 
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <Provider store={store}>
-    <style global jsx>{`
-    html,
-    body,
-    body > div:first-child,
-    div#__next,
-    div#__next > div {
-      height: 100%;
-      background: #034143;
-      background: -webkit-linear-gradient(top left, #034143, #B64A2F);
-      background: -moz-linear-gradient(top left, #034143, #B64A2F);
-      background: linear-gradient(to bottom right, #034143, #B64A2F);
-    };
-    }
+const MyApp = ({Component, pageProps}) => {
+    return (
+        <Provider store={store}>
+            <style global jsx>{
+                `
+                    html,
+                    body,
+                    body > div:first-child,
+                    div#__next,
+                    div#__next > div {
+                      height: 100%;
+                      background: #034143;
+                      background: -webkit-linear-gradient(top left, #034143, #B64A2F);
+                      background: -moz-linear-gradient(top left, #034143, #B64A2F);
+                      background: linear-gradient(to bottom right, #034143, #B64A2F);
+                    };
+                }
   `}</style>
-      <Component {...pageProps} />
-    </Provider>
-  )
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
 
 const makestore = () => store
